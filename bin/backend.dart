744e885257b17caedf343a40d61bbd82518f86a4
Dilawar Sumra <admin@ecosperityindia.com>
2023-05-29 15:19:59 +0530

    050523

--- bin/backend.dart
+++ bin/backend.dart
@@ -8,28 +8,28 @@ import 'package:shelf_router/shelf_router.dart';
 import 'package:shelf_web_socket/shelf_web_socket.dart';
 
 void main(List<String> arguments) async {
-  final ip = InternetAddress.anyIPv4;
-  var app = Router();
-
-  final overrideHeaders = {
-    'ACCESS_CONTROL_ALLOW_HEADERS': 'localhost',
-    'Content-Type': 'application/json;charset=utf-8',
-  };
-
-  final handler = const Pipeline()
-      .addMiddleware(
-        corsHeaders(
-          headers: overrideHeaders,
-        ),
-      )
-      .addHandler(app);
-
-  app.mount('/api/', RiderApi().router);
-
-  final port = int.parse(Platform.environment['PORT'] ?? '8080');
-  var server = await shelf_io.serve(handler, ip, port);
-  server.autoCompress = true;
-  print('Serving at http://${server.address.host}:${server.port}');
+  // final ip = InternetAddress.anyIPv4;
+  // var app = Router();
+  //
+  // final overrideHeaders = {
+  //   'ACCESS_CONTROL_ALLOW_HEADERS': 'localhost',
+  //   'Content-Type': 'application/json;charset=utf-8',
+  // };
+  //
+  // final handler = const Pipeline()
+  //     .addMiddleware(
+  //       corsHeaders(
+  //         headers: overrideHeaders,
+  //       ),
+  //     )
+  //     .addHandler(app);
+  //
+  // app.mount('/api/', RiderApi().router);
+  //
+  // final port = int.parse(Platform.environment['PORT'] ?? '8080');
+  // var server = await shelf_io.serve(handler, ip, port);
+  // server.autoCompress = true;
+  // print('Serving at http://${server.address.host}:${server.port}');
 
 /**************************************************************/
 
