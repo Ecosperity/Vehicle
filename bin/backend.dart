9a3a72cd18e22e34389640f5b22e5b40342b0052
Dilawar Sumra <admin@ecosperityindia.com>
2023-05-29 15:16:53 +0530

    050523

--- bin/backend.dart
+++ bin/backend.dart
@@ -1,9 +1,11 @@
 import 'dart:io';
 import 'package:backend/api/rider_api.dart';
 import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart';
 import 'package:shelf_cors_headers/shelf_cors_headers.dart';
 import 'package:shelf/shelf_io.dart' as shelf_io;
 import 'package:shelf_router/shelf_router.dart';
+import 'package:shelf_web_socket/shelf_web_socket.dart';
 
 void main(List<String> arguments) async {
   final ip = InternetAddress.anyIPv4;
@@ -28,4 +30,25 @@ void main(List<String> arguments) async {
   var server = await shelf_io.serve(handler, ip, port);
   server.autoCompress = true;
   print('Serving at http://${server.address.host}:${server.port}');
+
+/**************************************************************/
+
+  Handler _wsHandler = webSocketHandler((webSocket) {
+    webSocket.stream.listen((message) {
+      webSocket.sink.add("echo $message");
+    });
+  });
+
+  // Configure routes.
+  final _router = Router()..get('/api/v1/users/ws', _wsHandler);
+
+  final wsIp = InternetAddress.anyIPv4;
+// Configure a pipeline that logs requests.
+  final wsHandler = Pipeline().addMiddleware(logRequests()).addHandler(_router);
+
+  // For running in containers, we respect the PORT environment variable.
+  final wsPort = int.parse(Platform.environment['PORT'] ?? '8080');
+
+  final wsServer = await serve(wsHandler, wsIp, wsPort);
+  print('Server listening on port ${wsServer.port}');
 }
